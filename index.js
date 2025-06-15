c2cda180c7760a2a4d3a400992c38f0fab7f5a76
Bipana-Rai <bipanarai982@gmail.com>
2025-06-15 17:45:20 +0545

    d

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
+"https://resturant-admin-ten.vercel.app"
 ];
 
 const server = http.createServer(app);
